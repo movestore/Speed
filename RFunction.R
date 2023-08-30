3864cb0c867d07734bd6be6842b3cf33cdd83bf7
annescharf <ascharf@ab.mpg.de>
2023-08-30 22:46:48 +0200

    fix path to artifact

--- RFunction.R
+++ RFunction.R
@@ -5,24 +5,23 @@ library("units")
 
 
 rFunction <-  function(data, units=units) {
-
-    data$speed <- mt_speed(data, units=units)
-    
-    if(mt_n_tracks(data)==1){
-      speedHist <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Speed")+theme_bw()
-      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "speed_histogram.pdf"))
+  
+  data$speed <- mt_speed(data, units=units)
+  
+  if(mt_n_tracks(data)==1){
+    speedHist <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Speed")+theme_bw()
+    pdf(appArtifactPath("speed_histogram.pdf"))
+    print(speedHist)
+    dev.off()
+  } else {
+    pdf(appArtifactPath("speed_histogram.pdf"))
+    speedHistAll <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+ xlab("Speed")+ggtitle("All Individuals")+ theme_bw()
+    print(speedHistAll)
+    lapply(split(data, mt_track_id(data)), function(dataI){
+      speedHist <- ggplot(dataI[!is.na(dataI$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(dataI)]])+ xlab("Speed")+theme_bw()
       print(speedHist)
-      dev.off()
-    } else {
-      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "speed_histogram.pdf"))
-      speedHistAll <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+ xlab("Speed")+ggtitle("All Individuals")+ theme_bw()
-      print(speedHistAll)
-      lapply(split(data, mt_track_id(data)), function(dataI){
-        speedHist <- ggplot(dataI[!is.na(dataI$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(dataI)]])+ xlab("Speed")+theme_bw()
-        print(speedHist)
-      })
-      dev.off() 
-    
+    })
+    dev.off() 
   }
   return(data)
 }
