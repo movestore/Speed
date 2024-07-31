39ab2959cbcfe09c0c4bd6bbe0c2217a951b2f71
annescharf <ascharf@ab.mpg.de>
2024-07-31 21:02:15 +0200

    removing tracks with 1 loc

--- RFunction.R
+++ RFunction.R
@@ -7,18 +7,21 @@ library("units")
 rFunction <-  function(data, units=units) {
   
   data$speed <- mt_speed(data, units=units)
+  dataPlot <- data
+  dataPlot <- dataPlot[!is.na(dataPlot$speed),] # removing NA to ensure that indiv with only one loc ie speed NA get excluded
   
   if(mt_n_tracks(data)==1){
-    speedHist <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Speed")+theme_bw()
+    speedHist <- ggplot(dataPlot, aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(dataPlot)]])+ xlab("Speed")+theme_bw()
     pdf(appArtifactPath("speed_histogram.pdf"))
     print(speedHist)
     dev.off()
   } else {
     pdf(appArtifactPath("speed_histogram.pdf"))
-    speedHistAll <- ggplot(data[!is.na(data$speed),], aes(speed))+geom_histogram(bins=100)+ xlab("Speed")+ggtitle("All Individuals")+ theme_bw()
+    speedHistAll <- ggplot(dataPlot, aes(speed))+geom_histogram(bins=100)+ xlab("Speed")+ggtitle("All Individuals")+ theme_bw()
     print(speedHistAll)
-    lapply(split(data, mt_track_id(data)), function(dataI){
-      speedHist <- ggplot(dataI[!is.na(dataI$speed),], aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(dataI)]])+ xlab("Speed")+theme_bw()
+    lapply(split(dataPlot, mt_track_id(dataPlot)), function(dataI){
+      print(unique(mt_track_id(dataI)))
+      speedHist <- ggplot(dataI, aes(speed))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(dataI)]])+ xlab("Speed")+theme_bw()
       print(speedHist)
     })
     dev.off() 
